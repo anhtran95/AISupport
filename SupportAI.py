9cc17535425b1f3c541b0606ef84fdea3c4f1ce6
anhtran95 <tranngoc_baoanh2001@yahoo.com>
2020-11-09 17:24:21 -0700

    adding new options

--- SupportAI.py
+++ SupportAI.py
@@ -1,5 +1,7 @@
 import speech_recognition
 import pyttsx3
+import os
+import webbrowser
 from datetime import date, datetime
 
 #Initialize
@@ -7,6 +9,7 @@ robot_ear = speech_recognition.Recognizer()
 robot_mouth = pyttsx3.init()
 robot_brain = ""
 today = date.today()
+url = 'https://www.google.com/'
 
 
 
@@ -19,11 +22,12 @@ print("Robot: ...")
 
 try:
     you = robot_ear.recognize_google(audio)
+    you = you.lower()
 except:
-    you = ""
+    you = "Unable to pick up audio"
+
 
 print("You: " + you)
-
 
 #Interpretation of speech
 if you == "":
@@ -34,10 +38,18 @@ elif "today" in you:
     # Get date formatted: month, day and year
     today = date.today()
     robot_brain = today.strftime("%B %d, %Y")
-    #print("Date: " + formatted_date)
 elif "time" in you:
     now = datetime.now()
     robot_brain = now.strftime("%H hours %M minutes %S seconds")
+elif "sleep" in you:
+    print("Computer going to sleep mode...")
+    os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
+elif "chrome" in you:
+    chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+    print(chrome_path)
+    webbrowser.get(chrome_path).open(url)
+elif "mute" in you:
+    print("Mute, yet to implement")
 else:
     robot_brain = "That's great"
 
